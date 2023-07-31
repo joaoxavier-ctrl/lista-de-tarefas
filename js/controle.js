7110d0c6b081f6e42b0c7c9b869e38f4814c1c4f
JoaoXavierWin <joao.xavier21@gmail.com>
2023-07-31 03:19:46 -0300

    js corrigido(problema no id js)

--- js/controle.js
+++ js/controle.js
@@ -11,11 +11,11 @@ function addTarefa(){
     if((valorInput !=="") && (valorInput !== null) && (valorInput !== undefined)){
         ++contador;
 
-        let novoItem = `<div  id=${contador} class="item">
+        let novoItem = `<div  id="${contador}" class="item">
         <div onclick="marcarTarefa(${contador})" class="item-icone">
             <i id="icone${contador}" class="mid mdi-circle-outline"></i>
         </div>
-        <div onclick="marcarTarefa(${contador}) class="item-nome">
+        <div onclick="marcarTarefa(${contador})" class="item-nome">
             ${valorInput}
         </div>
         <div class="item-botao">
